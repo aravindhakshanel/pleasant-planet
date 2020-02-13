424f2545ba297827ea293bdbd9fa2a9247fc0322
dhakshan <aravindhakshane@gmail.com>
2020-02-14 02:30:00 +0530

    Update new.js

--- src/templates/new.js
+++ src/templates/new.js
@@ -1,41 +1,18 @@
-import React from "react";
-import Slider from "react-slick";
-import _ from 'lodash';
- 
-export default class SimpleSlider extends React.Component {
-  render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-        <div>
-            <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-      </div>
-    );
-  }
+import React from 'react';
+import Slider from 'react-slider';
+export default class Sideview extends React.Component{
+    render(){
+        return(
+            <Slider 
+            speed={500}
+            slidesToShow={1}
+            slidesToScroll={1}
+            infinite={false}
+            >
+                <Page>one</Page>
+                <Page>two</Page>
+                <Page>three</Page>
+            </Slider>
+        )
+    }
 }
